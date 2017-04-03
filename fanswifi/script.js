38ddc5468d58376311b8d2f949bfa76a68a9bbfe
Jackie Chui <jshchui@gmail.com>
2017-04-03 22:50:29 +0800

    deleted some stuff, edited a bit of the layout and font, needs more work later

--- fanswifi/script.js
+++ fanswifi/script.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
-
   //Hamburger menu toggling
   $('.click').click(function() {
     $(`.click`).toggleClass('change');
   });
-
-
 
   //Scrolling animation and color change
   var prevScroll = 0;
@@ -13,6 +10,7 @@ $(document).ready(function() {
   $(window).scroll(function() {
     currentScroll = $(window).scrollTop();
 
+    //nav goes up during downscroll, comes out when upscroll
     if(currentScroll < prevScroll) {
       $('#nav-bar').css("-webkit-transform", 'translateY(0)');
     } else  {
@@ -20,14 +18,11 @@ $(document).ready(function() {
     }
     prevScroll = currentScroll;
 
+    //nav is cleared at the top
     if(currentScroll > 500) {
       $('#nav-bar').css("background-color", "#F2631E");
     } else {
       $('#nav-bar').css("background-color", "transparent");
     }
   });
-
-  
-
-
 });
