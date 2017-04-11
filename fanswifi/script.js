e3af9acff239ef811e8970c3cab9c0cf83bdc144
Jackie Chui <jshchui@gmail.com>
2017-04-11 18:00:07 +0800

    IE should be good now, upgraded some images and some few fixes. theres code for animatoin but probably wont use it

--- fanswifi/script.js
+++ fanswifi/script.js
@@ -15,15 +15,54 @@ $(document).ready(function() {
     if(currentScroll < prevScroll) {
       $('#nav-bar').css("-webkit-transform", 'translateY(0)');
     } else  {
-      $('#nav-bar').css("-webkit-transform", "translateY(-60px)");
+      $('#nav-bar').css("-webkit-transform", "translateY(-70px)");
     }
     prevScroll = currentScroll;
 
     //nav is cleared at the top
-    if(currentScroll > 300) {
+    if(currentScroll > 400) {
       $('#nav-bar').css("background-color", "#F2631E");
     } else {
       $('#nav-bar').css("background-color", "transparent");
     }
   });
+
+
+
+
+
+  // (function($) {
+  //   $.fn.visible = function(partial) {
+  //       var $t            = $(this),
+  //           $w            = $(window),
+  //           viewTop       = $w.scrollTop(),
+  //           viewBottom    = viewTop + $w.height(),
+  //           _top          = $t.offset().top,
+  //           _bottom       = _top + $t.height(),
+  //           compareTop    = partial === true ? _bottom : _top,
+  //           compareBottom = partial === true ? _top : _bottom;
+  //     return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  //   };
+  // })(jQuery);
+  //
+  // var win = $(window);
+  // var allMods = $(".slide");
+  //
+  // allMods.each(function(i, el) {
+  //   var el = $(el);
+  //   if (el.visible(true)) {
+  //     el.addClass("already-visible");
+  //   }
+  // });
+  //
+  // win.scroll(function(event) {
+  //
+  //   allMods.each(function(i, el) {
+  //     var el = $(el);
+  //     if (el.visible(true)) {
+  //       el.addClass("come-in");
+  //     }
+  //   });
+  //
+  // });
 });
