2b66b305aa4376f1e92f4f72710446fc393d11a6
Jackie Chui <jshchui@gmail.com>
2017-04-29 22:53:12 +0800

    everything

--- fanswifi/js/script.js
+++ fanswifi/js/script.js
@@ -1,28 +1,60 @@
 $(document).ready(function() {
   //var dropdown = false;
-  //Hamburger menu toggling
+  //Hamburger menu toggling]
+  var open = false;
+  var prevScroll = 0;
+  var currentScroll =0;
   $('#hamburger').click(function(e) {
     $('#hamburger').parent().toggleClass('change');
-    $('#nav-bar').css("background-color", "#F2631E");
+    ///$('#nav-bar').parent().toggleClass('change');
+    if(open == false) {
+      $('#nav-bar').css("background-color", "#F2631E");
+      open = true;
+    } else {
+      if(currentScroll > 300) {
+        $('#nav-bar').css("background-color", "#F2631E");
+        open = false;
+      } else {
+        $('#nav-bar').css("background-color", "transparent");
+        open = false;
+      }
+    }
     //dropdown = !dropdown;
     console.log("this cannnot be running");
   });
 //&& e.target.parentNode.id !== 'hamburger'
   $(document).click(function(e) {
-    //console.log( dropdown);
+    console.log(e.target);
      if($('#click').hasClass('change')) {
       //if(dropdown === true) {
-      if (e.target.id != 'hamburger' && e.target.parentNode.id !== 'hamburger' && e.target.id != 'nav-content') {
+      if (e.target.id != 'hamburger' && e.target.parentNode.id !== 'hamburger' && e.target.id != 'nav-content' && $(e.target).attr('class') !== "expander")  {
         $('#hamburger').parent().toggleClass('change');
+        //$('#nav-bar').parent().toggleClass('change');
+        if(open == false) {
+          $('#nav-bar').css("background-color", "#F2631E");
+          open = true;
+        } else {
+          if(currentScroll > 300) {
+            $('#nav-bar').css("background-color", "#F2631E");
+            open = false;
+          } else {
+            $('#nav-bar').css("background-color", "transparent");
+            open = false;
+          }
+        }
         console.log('toggle menu');
         //dropdown = false;
       }
     }
   });
 
+  $('.expander').click(function(e)  {
+    $('.dropdown-content').toggleClass('show');
+  });
+
+
   //Scrolling animation and color change
-  var prevScroll = 0;
-  var currentScroll =0;
+
   $(window).scroll(function() {
     currentScroll = $(window).scrollTop();
 
@@ -40,8 +72,10 @@ $(document).ready(function() {
     //nav is cleared at the top
     if(currentScroll > 300) {
       $('#nav-bar').css("background-color", "#F2631E");
+      //$('#nav-bar').parent().toggleClass('change');
     } else {
       $('#nav-bar').css("background-color", "transparent");
+      //$('#nav-bar').parent().toggleClass('change');
     }
   });
 });
